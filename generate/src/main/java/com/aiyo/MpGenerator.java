347e4be005348465a831c920fc3a93e2d8d2e3b3
ylc <2550767776@qq.com>
2020-06-28 22:39:03 +0800

    docker编写

--- generate/src/main/java/com/aiyo/MpGenerator.java
+++ generate/src/main/java/com/aiyo/MpGenerator.java
@@ -1,12 +1,9 @@
 package com.aiyo;
 
-import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
-import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 import java.util.ArrayList;
@@ -22,16 +19,12 @@ public class MpGenerator {
     private static final String AUTHOR = "ylc";
     // 数据库相关
     private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/aiyo_system?characterEncoding=utf8";
+    private static final String DB_URL = "jdbc:mysql://192.168.1.201:3306/wxsaas_basic?characterEncoding=utf8";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "123456";
+    private static final String DB_PASSWORD = "wxkj147258!@#";
     private static final String PACKAGE_NAME = "com.aiyo";
     // 项目地址
-    private static final String PROJECT_DIR = "/home/ylc/Desktop/project/aiyo-system-root";
-    // 需要生成的表
-    private static final String[] INCLUDE_TABLES_ARR = new String[]{};
-    // 需要排除的表
-    private static final String[] EXCLUDE_TABLES_ARR = new String[]{};
+    private static final String PROJECT_DIR = "D:\\develop_project\\generate";
 
     /**
      * MySQL 生成
@@ -41,7 +34,7 @@ public class MpGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir(PROJECT_DIR + "/generate/src/main/java");
+        gc.setOutputDir(PROJECT_DIR + "\\src\\main\\java");
         gc.setFileOverride(true);//是否覆盖已有文件
         gc.setActiveRecord(true);//开启 ActiveRecord 模式
         gc.setEnableCache(false);// XML 二级缓存
@@ -52,19 +45,18 @@ public class MpGenerator {
         // 自定义文件命名，注意 %s 会自动填充表实体属性！
         // gc.setMapperName("%sDao");
         // gc.setXmlName("%sDao");
-        // gc.setServiceName("MP%sService");
+        gc.setServiceName("%sService");
         // gc.setServiceImplName("%sServiceDiy");
         // gc.setControllerName("%sAction");
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setDbType(DbType.MYSQL);
         dsc.setDriverName(DB_DRIVER);
         dsc.setUrl(DB_URL);
         dsc.setUsername(DB_USERNAME);
         dsc.setPassword(DB_PASSWORD);
-        dsc.setTypeConvert(new MySqlTypeConvert() {
+/*        dsc.setTypeConvert(new MySqlTypeConvert() {
             // 自定义数据库表字段类型转换【可选】
             @Override
             public DbColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
@@ -80,41 +72,26 @@ public class MpGenerator {
                 // 注意！！processTypeConvert 存在默认类型转换，如果不是你要的效果请自定义返回、非如下直接返回。
                 return (DbColumnType) super.processTypeConvert(globalConfig, fieldType);
             }
-        });
+        });*/
         mpg.setDataSource(dsc);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
         // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
-        strategy.setTablePrefix("t_", "");// 此处可以修改为您的表前缀
+        String[] tablePrefix = {"dic_", "c_", "m_"};
+        strategy.setTablePrefix(tablePrefix);// 此处可以修改为您的表前缀
         strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-        if (INCLUDE_TABLES_ARR.length > 0) {
+        // todo 配置需要生成的表
+        String[] includeArr = {"dic_base_dictionary"};
+        String[] excludeArr = {};
+        if (includeArr.length > 0) {
             // 需要生成的表
-            strategy.setInclude(INCLUDE_TABLES_ARR);
-        } else if (EXCLUDE_TABLES_ARR.length > 0) {
+            strategy.setInclude(includeArr);
+        } else if (excludeArr.length > 0) {
             // 排除生成的表
-            strategy.setExclude(EXCLUDE_TABLES_ARR);
+            strategy.setExclude(excludeArr);
         }
 
-        // 自定义实体父类
-        // strategy.setSuperEntityClass("com.baomidou.authentication.TestEntity");
-        // 自定义实体，公共字段
-        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
-        // 自定义 mapper 父类
-        // strategy.setSuperMapperClass("com.baomidou.authentication.TestMapper");
-        // 自定义 service 父类
-        // strategy.setSuperServiceClass("com.baomidou.authentication.TestService");
-        // 自定义 service 实现类父类
-        // strategy.setSuperServiceImplClass("com.baomidou.authentication.TestServiceImpl");
-        // 自定义 controller 父类
-        //strategy.setSuperControllerClass("com.xiaobingby.common.base.BaseController");
-        // 【实体】是否生成字段常量（默认 false）
-        // public static final String ID = "test_id";
-        // strategy.setEntityColumnConstant(true);
-        // 【实体】是否为构建者模型（默认 false）
-        // public User setName(String name) {this.name = name; return this;}
-        // strategy.setEntityBuilderModel(true);
-        // 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
         strategy.setEntityLombokModel(true);
         //开启前端控制器为Rest
         strategy.setRestControllerStyle(true);
@@ -129,7 +106,6 @@ public class MpGenerator {
         pc.setService("service");
         pc.setServiceImpl("service.impl");
 
-        //pc.setModuleName("test");
         mpg.setPackageInfo(pc);
 
         // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
@@ -148,7 +124,7 @@ public class MpGenerator {
         focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
-                return PROJECT_DIR + "/generate/src/main/resources/mapper/" + tableInfo.getEntityName() + "Mapper.xml";
+                return PROJECT_DIR + "\\src\\main\\resources\\mapper\\" + tableInfo.getEntityName() + "Mapper.xml";
             }
         });
         cfg.setFileOutConfigList(focList);
@@ -159,23 +135,9 @@ public class MpGenerator {
         tc.setXml(null);
         mpg.setTemplate(tc);
 
-        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
-        // 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
-        // TemplateConfig tc = new TemplateConfig();
-        // tc.setController("...");
-        // tc.setEntity("...");
-        // tc.setMapper("...");
-        // tc.setXml("...");
-        // tc.setService("...");
-        // tc.setServiceImpl("...");
-        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
-        // mpg.setTemplate(tc);
-
         // 执行生成
         mpg.execute();
 
-        // 打印注入设置【可无】
-        System.err.println(mpg.getCfg().getMap().get("abc"));
     }
 
 }
